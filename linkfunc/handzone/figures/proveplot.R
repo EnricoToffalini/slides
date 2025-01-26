ee0eca59d8bbd1b2c29c87a898fdf931f60d4c5a
EnricoToffalini <enrico.toffalini@unipd.it>
2025-01-26 11:17:21 +0100

    update

--- linkfunc/handzone/figures/proveplot.R
+++ linkfunc/handzone/figures/proveplot.R
@@ -6,46 +6,52 @@ library(effects)
 
 ################################
 
-# category group conditions
+# 2 x 2 categories
 
-N = 1e4
-conditionA = rbinom(N,1,.5)
-conditionB = rbinom(N,1,.5)
-group = paste(paste0("A",conditionA),paste0("B",conditionB))
+N = 5e4
+A = rbinom(N,1,.5)
+B = rbinom(N,1,.5)
 
-b0 = 0.5
-b1 = 0.5
-b2 = 1
+b0 = 1
+b1 = 1
+b2 = 0.5
 
-yLin = b0 + conditionA*b1 + conditionB*b2
+yLin = b0 + A*b1 + B*b2
 shape=5
-y = rgamma(N,shape=shape,scale=exp(yLin)/shape)
-df = data.frame(y,conditionA,conditionB,group)
-df$group = as.factor(df$group)
-df$conditionA = as.factor(df$conditionA)
-df$conditionB = as.factor(df$conditionB)
-ggplot(df)+geom_density(aes(x=y,group=group,color=group),linewidth=1)
+rt = rgamma(N,shape=shape,scale=20*exp(yLin))
+df = data.frame(rt,A,B)
+condition = paste(paste0("A",A),paste0("B",B))
+df$condition = as.factor(condition)
+df$A = as.factor(df$A)
+df$B = as.factor(df$B)
+ggplot(df)+
+  geom_density(aes(x=rt,group=condition,color=condition),linewidth=1.5)+
+  theme(text=element_text(size=24))+
+  coord_cartesian(xlim=c(min(df$rt),quantile(df$rt,.9975)))
 
-fit = glm(y~conditionA*conditionB,data=df,family=gaussian(link="log"))
+fit = glm(rt~A*B,data=df,family=gaussian(link="log"))
 summary(fit)
+eff = data.frame(allEffects(fit)$"A:B")
+ggplot(eff,aes(x=A,y=fit,group=B,color=B,shape=B,linetype=B))+
+  geom_point(size=6)+
+  geom_line(linewidth=1.5)+
+  geom_errorbar(aes(ymin=lower,ymax=upper),width=0)+
+  theme(text=element_text(size=24))+
+  ylab("rt")
 
-fit_log = glm(y~conditionA*conditionB,data=df,family=Gamma(link="log"))
-fit_id = glm(y~conditionA*conditionB,data=df,family=Gamma(link="identity"))
+fit_log = glm(rt~A*B,data=df,family=Gamma(link="log"))
+fit_id = glm(rt~A*B,data=df,family=Gamma(link="identity"))
 
-nd <- expand.grid(conditionA = c("0","1"), conditionB = c("0","1"))
+nd <- expand.grid(A = c("0","1"), B = c("0","1"))
 nd$p_log <- predict(fit_log, newdata = nd, type = "response")
 nd$p_id <- predict(fit_id, newdata = nd, type = "response")
 
-summary(fit)
-
-eff = data.frame(allEffects(fit)$"conditionA:conditionB")
-ggplot(eff,aes(x=conditionA,y=fit,group=conditionB,color=conditionB,shape=conditionB))+
-  geom_point(size=4)+geom_line(linewidth=1)+geom_errorbar(aes(ymin=lower,ymax=upper),width=0)
 
 df$sim = simulate(fit)$sim_1
 ggplot(df)+
-  geom_density(aes(x=y,group=group,color=group),linewidth=1)+
-  geom_density(aes(x=sim,group=group,fill=group),color=NA,alpha=.4)
+  geom_density(aes(x=rt,group=condition,color=condition),linewidth=1)+
+  geom_density(aes(x=sim,group=condition,fill=condition),color=NA,alpha=.4)+
+  theme(text=element_text(size=24))
 
 ################################
 
@@ -60,25 +66,25 @@ group = rbinom(N,1,.5)
 x = runif(N,0.5,3.5)
 yLin = b0 + b1*group + b2*x
 shape = 5
-y = rgamma(N,shape=shape,scale=exp(yLin)/shape)
-df = data.frame(y,x,group)
+rt = rgamma(N,shape=shape,scale=exp(yLin)/shape)
+df = data.frame(rt,x,group)
 df$group = as.factor(df$group)
 
-fit = glm(y~group*x,data=df,family=Gamma(link="identity"))
-#fit = glm(y~group*x,data=df,family=Gamma(link="log"))
+fit = glm(rt~group*x,data=df,family=Gamma(link="identity"))
+#fit = glm(rt~group*x,data=df,family=Gamma(link="log"))
 summary(fit)
 
 eff = data.frame(allEffects(fit,xlevels=list(x=seq(0.5,3.5,.1)))$"group:x")
 ggplot(eff,aes(x=x,group=group,color=group,y=fit))+
-  geom_point(data=df,aes(x=x,y=y),size=1,alpha=.2)+
+  geom_point(data=df,aes(x=x,y=rt),size=1,alpha=.2)+
   geom_line(linewidth=1)+
-  coord_cartesian(ylim=c(min(df$y),quantile(df$y,.999)))
+  coord_cartesian(ylim=c(min(df$rt),quantile(df$rt,.999)))
 
 df$sim = simulate(fit)$sim_1
 ggplot(df)+
-  geom_density(aes(x=y,group=group,color=group),linewidth=1)+
+  geom_density(aes(x=rt,group=group,color=group),linewidth=1)+
   geom_density(aes(x=sim,group=group,fill=group),color=NA,alpha=.4)
-# plot(df$y, df$sim)
+# plot(df$rt, df$sim)
 
 ####
 
@@ -95,12 +101,12 @@ for(i in 1:niter){
   x = runif(N,0.5,3.5)
   yLin = b0 + b1*group + b2*x
   shape = 5
-  y = rgamma(N,shape=shape,scale=yLin/shape)
-  df = data.frame(y,x,group)
+  rt = rgamma(N,shape=shape,scale=yLin/shape)
+  df = data.frame(rt,x,group)
   df$group = as.factor(df$group)
   
-  fitId = glm(y~group*x,data=df,family=Gamma(link="identity"))
-  fitLog = glm(y~group*x,data=df,family=Gamma(link="log"))
+  fitId = glm(rt~group*x,data=df,family=Gamma(link="identity"))
+  fitLog = glm(rt~group*x,data=df,family=Gamma(link="log"))
   deltaLog[i] = cv.glm(df,fitLog,K=10)$delta[2]
   deltaId[i] = cv.glm(df,fitId,K=10)$delta[2]
 }
